fede2571daab7bb1651d066c519e6c015d535643
1newchoppa <y.m.solodovnikov@student.khai.edu>
2022-10-17 23:47:59 +0300

    Hangman 2-st stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,3 +2,13 @@
 print('HANGMAN')
 print('The game will be available soon.')
 
+#Step 2
+word = ("php")
+user_input = input('Guess the word:')
+while True:
+    if user_input.strip() == word:
+        print('You survived!')
+        break
+    else:
+        print('You lost!')
+        break
