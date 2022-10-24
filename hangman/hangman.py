b05f8e801292b675552571ef2d2729b97f298122
1newchoppa <y.m.solodovnikov@student.khai.edu>
2022-10-24 19:13:58 +0300

    Hangman 5-st stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,15 +3,23 @@ import random
 
 print('HANGMAN')
 
-#Step 4
+#Step 5
+lifes = 8
 word_list = ["php" , "python" , "java" , "javascript"]
 word_random = random.choice(word_list)
-tire = (len(word_random)-3) * '-'
-user_input = input('Guess the word: ' + word_random[0:2] + tire + "-\n")
-while True:
-    if user_input.strip() == word_random:
-        print('You survived!')
-        break
+used = []
+letters = list(set(word_random))
+word_correct = "".join([i if i in used else "_" for i in word_random])
+print(word_correct)
+while lifes > 0:
+    input_letter = input("Input a letter")
+    if input_letter in word_random and input_letter not in used:
+        used.append(input_letter)
+        letters.remove(input_letter)
     else:
-        print('You lost!')
+        lifes -= 1
+    word_correct = "".join([i if i in used else "_" for i in word_random])
+    print(word_correct)
+    if len(letters) == 0:
         break
+print('Thanks for playing!\n We`ll see how well you did the next stage')
