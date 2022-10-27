16a74daf986f46e510df2c18b5557c8c7b7e8751
1newchoppa <y.m.solodovnikov@student.khai.edu>
2022-10-27 10:54:55 +0300

    Dinnerparty 2-st stage

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,9 +1,21 @@
-#Step 1
+import sys
+#Step 2
+
 friends_dict = {}
+
 num = int(input("Enter the number of friends joining (including you): "))
 if num < 1:
     print("No one is joining for the party")
+    sys.exit()
+
+
+print("Enter the name of every friend (including you),each on new line: ")
 for i in range(num):
-    friends_names = input("Enter the name of every friend (including you),each on new line: ")
+    friends_names = input()
     friends_dict[friends_names] = 0
+
+total_price = int(input("Enter the total amount"))
+mq = round(total_price/num, 2)
+for friends_names in friends_dict:
+    friends_dict[friends_names] = mq
 print(friends_dict)
