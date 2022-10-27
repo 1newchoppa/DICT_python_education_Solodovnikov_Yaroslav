c4c68ba52c0fa74b56ce361c448878e7654d4226
1newchoppa <y.m.solodovnikov@student.khai.edu>
2022-10-27 11:12:17 +0300

    Dinnerparty 3-st stage

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,5 +1,6 @@
 import sys
-#Step 2
+import random
+#Step 3
 
 friends_dict = {}
 
@@ -14,8 +15,16 @@ for i in range(num):
     friends_names = input()
     friends_dict[friends_names] = 0
 
+lucky_feature = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+lucky_one = ""
+if lucky_feature == 'Yes':
+    lucky_one = random.choice(list(friends_dict.keys()))
+    print(lucky_one, "is lucky today!")
+else:
+    print("No one is going to be lucky")
+
 total_price = int(input("Enter the total amount"))
 mq = round(total_price/num, 2)
 for friends_names in friends_dict:
     friends_dict[friends_names] = mq
-print(friends_dict)
+#print(friends_dict)
