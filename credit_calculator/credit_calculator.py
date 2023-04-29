edc427035c4b37e822c2a96421dcc54081bae90f
1newchoppa <y.m.solodovnikov@student.khai.edu>
2023-04-29 20:35:35 +0300

    Credit calculator 2-nd stage

--- credit_calculator/credit_calculator.py
+++ credit_calculator/credit_calculator.py
@@ -1,1 +1,29 @@
-print("Loan principal: 1000\nMonth 1: repaid 250\nMonth 2: repaid 250\nMonth 3: repaid 500\nThe loan has been repaid!")
+import math
+
+command_list = ['m','p']
+loan_sum = int(input("Enter the loan principal\n>"))
+option = input("What do you want to calculate?"
+               "\ntype 'm' – for number of monthly payments,"
+               "\ntype 'p' – for the monthly payment"
+               "\n>")
+
+
+
+def m():                             #кол-во месяцев для погашения
+    m_p = int(input("Enter the monthly payment\n>"))
+    months = math.ceil(loan_sum / m_p)
+    print(months)
+
+def p():                              #ежемесячный платёж
+    month_number = int(input("Enter the number of months:\n>"))
+    payment = math.ceil(loan_sum / month_number)
+    if payment * month_number > loan_sum:
+        last_payment = loan_sum - (month_number - 1) * payment
+        print(f"Your monthly payment = {payment} and the last payment = {last_payment}")
+    else:
+        print(f"Your monthly payment = {payment}")
+
+if option in command_list:
+    eval(option)()
+else:
+    print("Try again")
