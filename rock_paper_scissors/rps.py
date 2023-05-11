f2e8a3c0fd1ce02827bd70cbd8d1ae1b1b00014e
1newchoppa <y.m.solodovnikov@student.khai.edu>
2023-05-11 20:02:32 +0300

    Rock paper scissors 1-st stage

---
+++ rock_paper_scissors/rps.py
@@ -0,0 +1,9 @@
+user_input = input(">")
+if user_input == 'scissors':
+    print("Sorry, but the computer chose rock")
+elif user_input == 'paper':
+    print('Sorry, but the computer chose scissors')
+elif user_input == 'rock':
+    print("Sorry, but the computer chose paper")
+else:
+    print("Incorrect input")
