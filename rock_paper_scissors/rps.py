7d065e75be9a834284dafd3291cc1dc859870369
1newchoppa <y.m.solodovnikov@student.khai.edu>
2023-05-11 20:13:31 +0300

    Rock paper scissors 2-nd stage

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,9 +1,18 @@
+import random
+
+options = ["rock","paper","scissors"]
+
 user_input = input(">")
-if user_input == 'scissors':
-    print("Sorry, but the computer chose rock")
-elif user_input == 'paper':
-    print('Sorry, but the computer chose scissors')
-elif user_input == 'rock':
-    print("Sorry, but the computer chose paper")
-else:
+computer_choice = random.choice(options)
+
+if user_input not in options:
     print("Incorrect input")
+else:
+    if user_input == computer_choice:
+        print(f"There is a draw ({computer_choice})")
+    elif (user_input == 'rock' and computer_choice == 'scissors') or \
+            (user_input == 'paper' and computer_choice == 'rock') or \
+            (user_input == 'scissors' and computer_choice == 'paper'):
+        print(f"Well done. The computer chose {computer_choice} and failed")
+    else:
+        print(f"Sorry, but the computer chose {computer_choice}")
